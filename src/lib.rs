05220ebe2f8fb813c314034049adcb05dbf047eb
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-03-06 10:18:21 +0100

    Simplified code

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,19 @@
 //! Submodule providing the `Deletable` trait and the `User` struct.
 
-use diesel::{Connection, Identifiable};
-
 /// Trait representing an object that can be inserted into a table.
 pub trait Deletable: diesel::Identifiable + Copy
 where
     Self::Table: diesel::Table
         + 'static
-        + diesel::query_dsl::methods::FindDsl<<Self as Identifiable>::Id>
+        + diesel::query_dsl::methods::FindDsl<<Self as diesel::Identifiable>::Id>
         + diesel::query_builder::QueryId,
-    diesel::helper_types::Find<Self::Table, <Self as Identifiable>::Id>:
+    diesel::helper_types::Find<Self::Table, <Self as diesel::Identifiable>::Id>:
         diesel::query_builder::IntoUpdateTarget<Table = Self::Table>,
 {
+    /// Deletes the object from the database.
     fn delete<C: diesel::Connection>(&self, conn: &mut C) -> Result<bool, diesel::result::Error>
     where
-        <diesel::helper_types::Find<Self::Table, <Self as Identifiable>::Id> as diesel::query_builder::IntoUpdateTarget>::WhereClause:
+        <diesel::helper_types::Find<Self::Table, <Self as diesel::Identifiable>::Id> as diesel::query_builder::IntoUpdateTarget>::WhereClause:
             diesel::query_builder::QueryFragment<C::Backend> + diesel::query_builder::QueryId,
         <Self::Table as diesel::QuerySource>::FromClause: diesel::query_builder::QueryFragment<C::Backend>,
         C::Backend: diesel::backend::DieselReserveSpecialization
@@ -32,9 +31,9 @@ where
     U: diesel::Identifiable + Copy,
     U::Table: diesel::Table
         + 'static
-        + diesel::query_dsl::methods::FindDsl<<U as Identifiable>::Id>
+        + diesel::query_dsl::methods::FindDsl<<U as diesel::Identifiable>::Id>
         + diesel::query_builder::QueryId,
-    diesel::helper_types::Find<U::Table, <U as Identifiable>::Id>:
+    diesel::helper_types::Find<U::Table, <U as diesel::Identifiable>::Id>:
         diesel::query_builder::IntoUpdateTarget<Table = U::Table>,
 {
 }
@@ -47,26 +46,22 @@ diesel::table! {
     }
 }
 
-#[derive(
-    diesel::Queryable,
-    diesel::QueryableByName,
-    PartialEq,
-    Eq,
-    diesel::Identifiable,
-    diesel::Selectable,
-    Debug,
-    Clone,
-    Hash,
-)]
+#[derive(diesel::Identifiable)]
 #[diesel(table_name = users)]
-#[diesel(primary_key(id))]
+/// A struct representing a user.
 pub struct User {
+    /// The user's id.
     id: i32,
+    /// The user's name.
     name: String,
 }
 
 impl User {
-    pub fn delete_me<C: Connection>(&self, conn: &mut C) -> Result<bool, diesel::result::Error>
+    /// Deletes the current user from the database.
+    pub fn delete_me<C: diesel::Connection>(
+        &self,
+        conn: &mut C,
+    ) -> Result<bool, diesel::result::Error>
     where
         C::Backend: diesel::backend::DieselReserveSpecialization,
         i32: diesel::serialize::ToSql<diesel::sql_types::Integer, C::Backend>,
